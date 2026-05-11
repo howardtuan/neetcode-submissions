1c5af92811e453c69368e12dc2417150f217b3aa
段浩恩 Howard Tuan <howardtuan@gmail.com>
2026-05-11 15:31:10 +0800

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        MyDict = {'(':')','{':'}','[':']'}
+        stack = []
+
+        for i in range(len(s)):
+            if s[i] == '(' or s[i] == '{' or s[i] == '[':
+                stack.append(s[i])
+            else:
+                if len(stack) == 0:
+                    return False
+
+                temp = stack.pop()
+                if s[i] != MyDict[temp]:
+                    return False
+
+        return len(stack) == 0
